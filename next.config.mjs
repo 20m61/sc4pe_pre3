3d0cb2195c56581bcc016f62d9d54bc6ff44851c
20m61 <ifm0s1m0s1m0nn@hotmail.com>
2024-08-17 10:40:05 +0000

    chore: Update Next.js deployment workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/sc4pe_pre3',
+    assetPrefix: '/sc4pe_pre3/',
 };
 
 export default nextConfig;
